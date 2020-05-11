0b3f3b24c7065e33fbc6a220dc6e667b7f4b9183
Jeremy Green <jeremy@octolabs.com>
2020-05-10 23:45:07 -0500

    Getting started with tree_graph

--- lib/trace_graph/trace_node.rb
+++ lib/trace_graph/trace_node.rb
@@ -1,7 +1,13 @@
+require 'tree_graph'
+
 module TraceGraph
   class TraceNode
+    include TreeGraph
     attr_accessor :label
     attr_accessor :sub_nodes
+
+    alias_method :label_for_tree_graph, :label
+    alias_method :children_for_tree_graph, :sub_nodes
 
     def initialize(label)
       self.label = label
